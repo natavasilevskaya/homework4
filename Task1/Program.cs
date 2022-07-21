974d34e8a2a2e860ab496b0f82e891918607fb99
natavasilevskaya <107160563+natavasilevskaya@users.noreply.github.com>
2022-07-22 01:03:00 +0300

    Task1 file added

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа принимает на вход число и выдаёт сумму цифр в числе.
+
+int SumOfDigit (int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+
+    
+Console.WriteLine("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Sum of digit is " + SumOfDigit(number));
