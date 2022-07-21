f487b038e0f1ea90eda7cfbce60d3238346adced
natavasilevskaya <107160563+natavasilevskaya@users.noreply.github.com>
2022-07-22 02:07:10 +0300

    Task2 file added

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Програмама задаёт массив из 8 элементов и выводит их на экран.
+
+int[] numbers = new int[8];
+      
+void FillArray(int[] array)
+{
+    for (int i = 0; i<8; i++ )
+    {
+        array[i] = new Random().Next(9, 99);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i<8; i++ )
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine(array);
+}
+
+FillArray(numbers);
+PrintArray(numbers);
